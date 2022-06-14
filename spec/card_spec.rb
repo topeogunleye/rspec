3551267ea90dbc3d26fe789f77b149170498a0ae
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-06-14 14:03:16 +0100

    Add tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,3 +1,28 @@
-RSpec.describe 'Card' do
+class Card
+    
+  attr_reader :rank, :suit
+  
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
 
+RSpec.describe Card do
+
+  before(:each) do
+    @card = Card.new('Ace', 'Spades')
+  end
+
+  it 'has a type' do
+    
+  end
+  
+  it 'has a rank' do  
+    expect(@card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    expect(@card.suit).to eq ('Spades')
+  end
 end
